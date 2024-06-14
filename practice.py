14bc3e2198500d431826e3a3adb7bfef321bade2
unknown <ibrahimeenat24@gmail.com>
2024-06-14 08:22:16 +0100

    loop commit

---
+++ practice.py
@@ -0,0 +1,8 @@
+color = ['orange' , 'blue' ,' purple' , 'pink' , 'red' , 'black']
+for clr in color:
+#     print ([color])
+# else:
+#     print (len, color[1])
+
+for color_length = (color):
+print (color_length)
